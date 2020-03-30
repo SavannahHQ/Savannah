ac6b3d472c9d23ceda6383330115e8abc5c9631f
Michael Hall <mhall119@gmail.com>
2020-03-30 13:27:15 -0400

    Add top active users to dashboard

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,5 @@
+import operator
+import datetime
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
@@ -25,6 +27,16 @@ def dashboard(request, community_id):
     communities = Community.objects.filter(owner=request.user)
     community = get_object_or_404(Community, id=community_id)
     request.session['community'] = community_id
+    recent_conversations = Conversation.objects.filter(channel__source__community=community, timestamp__gte=datetime.datetime.now() - datetime.timedelta(days=7))
+    activity_counts = dict()
+    for c in recent_conversations:
+        for p in c.participants.all():
+            if p in activity_counts:
+                activity_counts[p] = activity_counts[p] + 1
+            else:
+                activity_counts[p] = 1
+    most_active = [(member, count) for member, count in sorted(activity_counts.items(), key=operator.itemgetter(1))]
+    most_active.reverse()
     try:
         user_member = Member.objects.get(user=request.user, community=community)
     except:
@@ -33,5 +45,6 @@ def dashboard(request, community_id):
         "communities": communities,
         "community": community,
         "user_member": user_member,
+        "most_active": most_active[:10],
     }
     return render(request, 'savannah/dashboard.html', context)
