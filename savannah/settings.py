a0c96c826d49d379f9fa8509a1c55517c5f6101e
Michael Hall <mhall119@gmail.com>
2020-05-08 10:51:38 -0400

    Remove old frontend app from settings

--- savannah/settings.py
+++ savannah/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'corm.apps.CormConfig',
-    'frontend.apps.FrontendConfig',
     'frontendv2.apps.FrontendConfig',
     'grimoire',
 ]
