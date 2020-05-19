c1cef829f6fc0188893f3e916823de34c1e83aa3
Michael Hall <mhall119@gmail.com>
2020-05-19 18:13:07 -0400

    Add checks for a returning user after 7 days of inactivity

--- corm/management/commands/activity_check.py
+++ corm/management/commands/activity_check.py
@@ -14,9 +14,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for community in Community.objects.all():
-            self.check_member_activity(community)
+            #self.check_for_inactivity(community)
+            self.check_for_resuming_activity(community)
 
-    def check_member_activity(self, community):
+    def check_for_inactivity(self, community):
         members = Member.objects.filter(community=community, last_seen__lte=datetime.datetime.utcnow() - datetime.timedelta(days=14), last_seen__gt=datetime.datetime.utcnow() - datetime.timedelta(days=15)).annotate(past_activity=Count('conversation', distint=True, filter=Q(conversation__timestamp__gte=datetime.datetime.utcnow() - datetime.timedelta(days=90))))
         for member in members:
             if member.past_activity >= 50:
@@ -26,6 +27,20 @@ class Command(BaseCommand):
                     recipient=recipients, 
                     verb="has been inactive since %s" % member.last_seen.date(),
                     level='warning',
+                    icon_name="fas fa-user",
+                    link=reverse('member_profile', kwargs={'member_id':member.id})
+                )
+
+    def check_for_resuming_activity(self, community):
+        members = Member.objects.filter(community=community, last_seen__gte=datetime.datetime.utcnow() - datetime.timedelta(days=1)).annotate(last_activity=Max('conversation__timestamp', filter=Q(conversation__timestamp__lt=datetime.datetime.utcnow() - datetime.timedelta(days=1))))
+        for member in members:
+            if member.last_activity is not None and member.last_activity < datetime.datetime.utcnow() - datetime.timedelta(days=7):
+                print("Member became active again: %s (previously seen %s)" % (member.name, member.last_activity))
+                recipients = community.managers or community.owner
+                notify.send(member, 
+                    recipient=recipients, 
+                    verb="has been active for the first time since %s" % member.last_activity.date(),
+                    level='success',
                     icon_name="fas fa-user",
                     link=reverse('member_profile', kwargs={'member_id':member.id})
                 )
