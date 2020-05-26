a31efed0d5b456d0676c6f2bb10af658196cb417
Michael Hall <mhall119@gmail.com>
2020-05-26 16:19:07 -0400

    Suggest member merges if contacts have matching email addresses

--- corm/management/commands/make_suggestions.py
+++ corm/management/commands/make_suggestions.py
@@ -16,10 +16,28 @@ class Command(BaseCommand):
             self.make_merge_suggestions(community)
 
     def make_merge_suggestions(self, community):
-        dups = Contact.objects.filter(member__community=community).values('detail').annotate(dup_count=Count('member_id', distinct=True)).order_by('detail').filter(dup_count__gt=1)
-        print("Found %s duplicates" % len(dups))
-        i = 0
         merge_count = 0
+        # Check for duplicate usernames
+        dups = Contact.objects.filter(member__community=community, email_address__isnull=False).values('email_address').annotate(dup_count=Count('member_id', distinct=True)).order_by('email_address').filter(dup_count__gt=1)
+        print("Found %s duplicate email addresses" % len(dups))
+        i = 0
+        for dup in dups:
+            if dup['dup_count'] > 1:
+                print("%s: %s" % (i, dup))
+                i += 1
+                members = Member.objects.filter(community=community, contact__email_address=dup['email_address']).order_by('id').distinct()
+                destination_member = members[0]
+                print("Target member: [%s] %s" % (destination_member.id, destination_member))
+                for source_member in members[1:]:
+                    print("    <- [%s] %s" % (source_member.id, source_member))
+                    suggestion, created = SuggestMemberMerge.objects.update_or_create(community=community, destination_member=destination_member, source_member=source_member, defaults={'reason':'Matching contact emails: %s' % dup['email_address']})
+                    if created:
+                        merge_count += 1
+
+        # Check for duplicate usernames
+        dups = Contact.objects.filter(member__community=community).values('detail').annotate(dup_count=Count('member_id', distinct=True)).order_by('detail').filter(dup_count__gt=1)
+        print("Found %s duplicate usernames" % len(dups))
+        i = 0
         for dup in dups:
             if dup['dup_count'] > 1:
                 print("%s: %s" % (i, dup))
@@ -32,6 +50,8 @@ class Command(BaseCommand):
                     suggestion, created = SuggestMemberMerge.objects.update_or_create(community=community, destination_member=destination_member, source_member=source_member, defaults={'reason':'Matching contact: %s' % dup['detail']})
                     if created:
                         merge_count += 1
+
+        # Notify managers of new suggestions
         print("Suggested %s member merges" % merge_count)
         if merge_count > 0:
             recipients = community.managers or community.owner
