50d9c01b55ac5801b8275969f4c8109424bb8f3e
Michael Hall <mhall119@gmail.com>
2020-08-12 10:20:36 -0400

    Tag conversations in chunks to reduce memory load

--- corm/management/commands/tag_conversations.py
+++ corm/management/commands/tag_conversations.py
@@ -20,18 +20,25 @@ class Command(BaseCommand):
             if not word in keywords:
               keywords[word] = set()
             keywords[word].add(tag)
-        #print("Found %s keywords" % len(keywords))
 
-        for convo in Conversation.objects.filter(channel__source__community=community):
-          table = str.maketrans(PUNCTUATION, ' '*len(PUNCTUATION))
-          if convo.content is None:
-            continue
-          text = " "+convo.content.lower().translate(table)+" "
-          tagged = set()
-          for keyword in keywords:
-            if keyword in text:
-              for tag in keywords[keyword]:
-                if tag.id not in tagged:
-                  #print("Tagging \"%s\" with %s because of %s" % (convo, tag.name, keyword))
-                  convo.tags.add(tag)
-                  tagged.add(tag.id)
+        conversations = Conversation.objects.filter(channel__source__community=community)
+        count = conversations.count()
+        page = 0
+        chunk = 10000
+        while count > (chunk * page):
+          start = chunk * page
+          end = start + chunk
+          for convo in Conversation.objects.filter(channel__source__community=community)[start:end]:
+            table = str.maketrans(PUNCTUATION, ' '*len(PUNCTUATION))
+            if convo.content is None:
+              continue
+            text = " "+convo.content.lower().translate(table)+" "
+            tagged = set()
+            for keyword in keywords:
+              if keyword in text:
+                for tag in keywords[keyword]:
+                  if tag.id not in tagged:
+                    #print("Tagging \"%s\" with %s because of %s" % (convo, tag.name, keyword))
+                    convo.tags.add(tag)
+                    tagged.add(tag.id)
+          page += 1
