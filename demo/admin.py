503d511f8727e8b259ba671c476e76ef110dc5c6
Michael Hall <mhall119@gmail.com>
2021-07-13 13:54:46 -0400

    Add view link to demonstrations admin

--- demo/admin.py
+++ demo/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from demo.models import Demonstration
 
 # Register your models here.
 class DemoAdmin(admin.ModelAdmin):
-    list_display = ["community", "status", "created", "expires"]
+    list_display = ["community", "link", "status", "created", "expires"]
     list_filter = ["status", "created", "expires"]
     search_fields = ["community__name", "owner__username"]
 
+    def link(self, demo):
+        return mark_safe("<a href=\"/dashboard/%s\" target=\"_blank\">View</a>" % demo.community.id)
+    link.short_description = "Dashboard"
 admin.site.register(Demonstration, DemoAdmin)
