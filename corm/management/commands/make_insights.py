44fe51e57f43ff643bdf36b500bd75288ad8e770
Michael Hall <mhall119@gmail.com>
2022-06-07 11:38:31 -0400

    Tweak trending tag insights

--- corm/management/commands/make_insights.py
+++ corm/management/commands/make_insights.py
@@ -66,7 +66,7 @@ class Command(BaseCommand):
             baseline_diff = 100 * (tag.insight_count - tag.baseline_count)/tag.baseline_count
             trend = (tag.trend_count / trend_days) * offset_days
             trend_diff = 100 * (tag.insight_count - trend)/trend
-            if baseline_diff > 10 and trend_diff > 50 and baseline_diff > trend_diff:
+            if baseline_diff > 10 and trend_diff > 50 and trend_diff > baseline_diff:
                 if self.verbosity >= 2:
                     print("%s [baseline]: %s -> %s (%s%%)" % (tag.name, tag.baseline_count, tag.insight_count, baseline_diff))
                     print("%s [trend]: %s -> %s (%s%%)" % (tag.name, trend, tag.insight_count, trend_diff))
