913f49729ddce64c44e30389730d484f93eb658f
Michael Hall <mhall119@gmail.com>
2022-06-10 12:52:19 -0400

    Add insight for a higher than normal increase in new members from a given source

--- corm/management/commands/make_insights.py
+++ corm/management/commands/make_insights.py
@@ -47,6 +47,63 @@ class Command(BaseCommand):
             self.make_source_insights(community)
             self.make_level_insights(community)
             self.make_tag_insights(community)
+            self.make_new_member_insights(community)
+
+    def make_new_member_insights(self, community):
+        offset_days = 7
+        trend_days = 60
+
+        trend_start = datetime.datetime.utcnow() - datetime.timedelta(days=(offset_days*2)+trend_days)
+        trend_end = baseline_start = datetime.datetime.utcnow() - datetime.timedelta(days=offset_days*2)
+        baseline_end = insight_start = datetime.datetime.utcnow() - datetime.timedelta(days=offset_days)
+        insight_end = datetime.datetime.utcnow()
+        sources = Source.objects.filter(community=community)
+        sources = sources.annotate(trend_count=Count('contact__member', filter=Q(contact__member__first_seen__gt=trend_start, contact__member__first_seen__lte=trend_end)))
+        sources = sources.annotate(baseline_count=Count('contact__member', filter=Q(contact__member__first_seen__gt=baseline_start, contact__member__first_seen__lte=baseline_end)))
+        sources = sources.annotate(insight_count=Count('contact__member', filter=Q(contact__member__first_seen__gt=insight_start, contact__member__first_seen__lte=insight_end)))
+        sources = sources.filter(baseline_count__gt=0, insight_count__gt=0)
+
+        for source in sources:
+            baseline_diff = 100 * (source.insight_count - source.baseline_count)/source.baseline_count
+            trend = (source.trend_count / trend_days) * offset_days
+            trend_diff = 100 * (source.insight_count - trend)/trend
+            if self.verbosity >= 2:
+                print("%s [baseline]: %s -> %s (%s%%)" % (source.name, source.baseline_count, source.insight_count, baseline_diff))
+                print("%s [trend]: %s -> %s (%s%%)" % (source.name, trend, source.insight_count, trend_diff))
+            if source.insight_count > 10 and baseline_diff > 25 and trend_diff > 50:
+                uid = 'new-members:%s' % source.id
+                try:
+                    last_insight = Insight.objects.filter(community=community, uid=uid).order_by('-timestamp')[0]
+                    days_since_last_insight = (datetime.datetime.utcnow() - last_insight.timestamp).days
+                    if days_since_last_insight < offset_days:
+                        return
+                except:
+                    pass
+                insight_title = 'New users coming in from <b>%s</b>' % source.connector_name
+                if source.name == source.connector_name:
+                    insight_text = '<p>Your community has added %s new members over the last %s days from <b>%s</b></p>' % (source.insight_count, offset_days, source.name, )
+                else:
+                    insight_text = '<p>Your community has added %s new members over the last %s days from <b>%s</b> on <b>%s</b>.</p>' % (source.insight_count, offset_days, source.name, source.connector_name)
+                insight_text += '<p>This is a %0.2f%% increase over previous %s days, and a %0.2f%% increase over the %s days prior to that.<p>' % (baseline_diff, offset_days, trend_diff, trend_days)
+
+                tags = Tag.objects.filter(community=community)
+                tags = tags.annotate(convo_count=Count('conversation', filter=Q(conversation__channel__source=source, conversation__speaker__first_seen__gte=insight_start)))
+                tags = tags.filter(convo_count__gte=5)
+                if tags.count() > 0:
+                    insight_text += "<p>Their primary topics were:<ul>"
+                    for tag in tags.order_by('-convo_count')[:10]:
+                        insight_text += '<li><span class="tag-pill text-nowrap" style="background-color: #%s; "><span class="tag-text" style="color: #%saa;">%s</span></span> (%s comments)</li>' % (tag.color, tag.color, tag.name, tag.convo_count)
+                    insight_text += "</ul></p>"
+                Insight.create(
+                    community=community, 
+                    recipient=community.managers or community.owner,
+                    uid=uid,
+                    level=Insight.InsightLevel.SUCCESS,
+                    title=insight_title,
+                    text=insight_text,
+                    cta="View Members",
+                    link=reverse('all_members', kwargs={'community_id':community.id})+"?clear=all&timespan=%s&sort=-first_seen&source=%s" % (offset_days, source.id)
+                )
 
     def make_tag_insights(self, community):
         offset_days = 7
@@ -66,7 +123,7 @@ class Command(BaseCommand):
             baseline_diff = 100 * (tag.insight_count - tag.baseline_count)/tag.baseline_count
             trend = (tag.trend_count / trend_days) * offset_days
             trend_diff = 100 * (tag.insight_count - trend)/trend
-            if baseline_diff > 10 and trend_diff > 50 and trend_diff > baseline_diff:
+            if tag.insight_count > 5 and baseline_diff > 10 and trend_diff > 50 and trend_diff > baseline_diff:
                 if self.verbosity >= 2:
                     print("%s [baseline]: %s -> %s (%s%%)" % (tag.name, tag.baseline_count, tag.insight_count, baseline_diff))
                     print("%s [trend]: %s -> %s (%s%%)" % (tag.name, trend, tag.insight_count, trend_diff))
@@ -91,6 +148,7 @@ class Command(BaseCommand):
                     cta="View Conversations",
                     link=reverse('conversations', kwargs={'community_id':community.id})+"?clear=all&timespan=90&tag=%s"%tag.name
                 )
+
     def make_level_insights(self, community):
         offset_days = 30
         try:
