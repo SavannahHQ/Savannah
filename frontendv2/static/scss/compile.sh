dd14708c85a719a472e2d1a859b95fa21310437a
Michael Hall <mhall119@gmail.com>
2022-08-06 16:25:19 -0400

    Don't use toptal to minify CSS

--- frontendv2/static/scss/compile.sh
+++ frontendv2/static/scss/compile.sh
@@ -3,6 +3,7 @@ echo "Compiling CSS..."
 sass sb-admin-2.scss ../css/sb-admin-2.css 
 
 echo "Minifying CSS..."
-curl -X POST -s --data-urlencode 'input@../css/sb-admin-2.css' https://www.toptal.com/developers/cssminifier/api/raw > ../css/sb-admin-2.min.css
+# curl -X POST -s --data-urlencode 'input@../css/sb-admin-2.css' https://www.toptal.com/developers/cssminifier/api/raw > ../css/sb-admin-2.min.css
+echo "Use https://www.cleancss.com/css-minify/ to minify"
 
 echo "Done."
